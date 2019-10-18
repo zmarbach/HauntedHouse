5768039f8cccf350fed794fad0ab86ef835061bb
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-10-18 10:20:13 -0500

    Finished Haunted House Builder and the Room Repository -- created tables in MySQL

--- src/main/java/org/improving/HauntedHouse/HauntedHouseBuilder.java
+++ src/main/java/org/improving/HauntedHouse/HauntedHouseBuilder.java
@@ -3,24 +3,33 @@ package org.improving.HauntedHouse;
 import org.improving.HauntedHouse.database.RoomRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class HauntedHouseBuilder {
     private RoomRepository roomRepository;
-    //private Room room;
+    private List<Room> roomList = new ArrayList<>();
 
+    //constructor
     public HauntedHouseBuilder(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
     }
 
-    // List<Room> getRoomList() { return roomList }
+    //getter
+    List<Room> getRoomList() {
+        return roomList;
+    }
 
-    // public Room buildHauntedHouse() {
-    // try {
-    // List<Room> rooms = roomRepository.findByName("The Foyer");
-    // roomList = rooms;
-    // return roomList.get(0);
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
-    // return null;
+    //method
+    public Room buildHauntedHouse() {
+        try {
+            List<Room> rooms = roomRepository.findByName("Room1");
+            roomList = rooms;
+            return roomList.get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/main/java/org/improving/HauntedHouse/database/RoomRepository.java
+++ src/main/java/org/improving/HauntedHouse/database/RoomRepository.java
@@ -1,9 +1,12 @@
 package org.improving.HauntedHouse.database;
 
+import org.improving.HauntedHouse.Room;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends CrudRepository {
-    // List<Room> findByName(String name);
+     List<Room> findByName(String name);
 }
