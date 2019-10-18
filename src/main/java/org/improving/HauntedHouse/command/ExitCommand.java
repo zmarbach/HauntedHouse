3a71828e933144ac360d8a941f9113429803a28e
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-10-18 13:35:36 -0500

    Created Exit Command

---
+++ src/main/java/org/improving/HauntedHouse/command/ExitCommand.java
@@ -0,0 +1,21 @@
+package org.improving.HauntedHouse.command;
+
+import org.improving.HauntedHouse.Game;
+import org.improving.HauntedHouse.exception.GameExitException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExitCommand implements Command {
+
+    @Override
+    public boolean isValid(String input, Game game) {
+        return input.equalsIgnoreCase("exit");
+    }
+
+    @Override
+    public void execute(String input, Game game) throws GameExitException {
+        System.out.println("Ah, well, it has been fun.");
+        throw new GameExitException();
+
+    }
+}
